db927f1e2c6f9af9befdd6f01c271df4c089fd9b
itachi <Sameer Deshpande>
2017-11-05 01:41:51 -0500

    L1 <-> L2 interactions correctly accounted and also DRAM access

--- cache_multiprocessor_memory_system/dram.cpp
+++ cache_multiprocessor_memory_system/dram.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <cmath>
 #include "dram.h"
 
 #define ROWBUF_SIZE         1024
@@ -102,6 +103,36 @@ uint64_t dram_access_sim_rowbuf(DRAM *dram, Addr lineaddr, Flag is_dram_write)
   // You need to write this fuction to track open rows
   // You will need to compute delay based on row hit/miss/empty
 
+  const uint32_t numLinesInRowBuf = ROWBUF_SIZE / CACHE_LINESIZE;
+  const uint32_t numOfLineBits = std::log2(numLinesInRowBuf);
+  const uint32_t numOfBankBits = std::log2(DRAM_BANKS);
+
+  const uint32_t lineNum = lineaddr & ((0x1 << numOfLineBits) - 1);
+  const uint32_t bankNum = (lineaddr >> numOfLineBits) & ((0x1 << numOfBankBits) - 1);
+
+  const uint64_t rowNum = (lineaddr >> (numOfBankBits + numOfLineBits));
+
+  Rowbuf_Entry& bankRowBuf = dram->perbank_row_buf[bankNum];
+  if (FALSE == bankRowBuf.valid)
+  {
+    bankRowBuf.valid = TRUE;
+    bankRowBuf.rowid = rowNum;
+    delay += DRAM_T_ACT + DRAM_T_CAS + DRAM_T_PRE;
+  }
+  else
+  {
+    if (rowNum == bankRowBuf.rowid)
+    {
+      delay += DRAM_T_CAS;
+    }
+    else
+    {
+      delay += DRAM_T_PRE + DRAM_T_CAS + DRAM_T_ACT;
+      bankRowBuf.rowid = rowNum;
+    }
+  }
+
+  delay += DRAM_T_BUS;
 
   return delay;
 }
